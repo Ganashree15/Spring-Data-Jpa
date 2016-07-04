425366100775872f870e082d6ec69248424afb3f
Ganashree15 <ganapari06@gmail.com>
2016-07-04 17:40:17 +0800

    Spring-Data-JPA Example

---
+++ src/main/java/com/spring/service/ProductService.java
@@ -0,0 +1,50 @@
+package com.spring.service;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spring.dao.ProductRepository;
+import com.spring.entity.Product;
+
+/** 
+ * Service layer.
+ * Specify transactional behavior and mainly
+ * delegate calls to Repository.
+ */
+@Component
+public class ProductService {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Transactional
+	public void add(Product product) {
+		productRepository.save(product);
+	}
+	
+	@Transactional(readOnly=true)
+	public List<Product> findAll() {
+		return productRepository.findAll();
+	}
+
+	@Transactional
+	public void addAll(Collection<Product> products) {
+		for (Product product : products) {
+			productRepository.save(product);
+		}
+	}
+
+	@Transactional(readOnly=true)
+	public List<Product> findByNameIs(String name) {
+		return productRepository.findByNameIs(name);
+	}
+	
+	@Transactional(readOnly=true)
+	public List<Product> findByNameContainingIgnoreCase(String searchString) {
+		return productRepository.findByNameContainingIgnoreCase(searchString);
+	}
+}
